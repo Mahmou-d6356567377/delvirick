ccfcb6a908aac3aa0132e1454944e0014c18a655
mohamedagm <abogm44ud@gmail.com>
2025-09-29 12:00:39 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:delvirick/utils/routes/go_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-
+//GMY BRANCH
 void main() {
     WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
